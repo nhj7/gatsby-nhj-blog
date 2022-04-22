4f1588640bcddad8df9a04244c4cae688e159f60
HyungJu.Na <nhj12311@gmail.com>
2022-04-22 23:03:45 +0900

    update : default image align center

--- src/components/post-container/index.jsx
+++ src/components/post-container/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import './index.scss'
+
 export const PostContainer = ({ html }) => (
-  <div dangerouslySetInnerHTML={{ __html: html }} />
+  <div id='PostContainer' dangerouslySetInnerHTML={{ __html: html }} />
 )
